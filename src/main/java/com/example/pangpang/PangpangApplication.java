236e515a6b4f845e0272e6faea474a7dfbda40cc
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-07-20 15:38:27 +0900

    first commit

---
+++ src/main/java/com/example/pangpang/PangpangApplication.java
@@ -0,0 +1,13 @@
+package com.example.pangpang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangpangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangpangApplication.class, args);
+	}
+
+}
