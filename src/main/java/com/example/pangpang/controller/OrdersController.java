58ed63c3d24cb4c7d0708b72c24d041a4129d7a7
unknown <leegj420@gmail.com>
2024-07-29 17:40:10 +0900

    오더 컨트롤러, 서비스에 추가와 목록보기 제작

--- src/main/java/com/example/pangpang/controller/OrdersController.java
+++ src/main/java/com/example/pangpang/controller/OrdersController.java
@@ -1,10 +1,39 @@
 package com.example.pangpang.controller;
 
+import java.util.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.pangpang.dto.OrdersDTO;
+import com.example.pangpang.service.OrdersService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrdersController {
     
+    private final OrdersService ordersService;
+
+
+    @GetMapping("/list")
+    public List<OrdersDTO> getList(){
+
+        return ordersService.list();
+
+    }
+
+
+    @PostMapping("/")
+    public Map<String, String> add(@RequestBody OrdersDTO ordersDTO){
+
+        ordersService.add(ordersDTO);
+
+        return Map.of("result", "ok");
+    }
 }
--- src/main/java/com/example/pangpang/service/OrdersService.java
+++ src/main/java/com/example/pangpang/service/OrdersService.java
@@ -1,8 +1,42 @@
 package com.example.pangpang.service;
 
-import org.springframework.stereotype.Service;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.pangpang.dto.OrdersDTO;
+import com.example.pangpang.entity.Orders;
+import com.example.pangpang.repository.OrdersRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Transactional
 @Service
+@RequiredArgsConstructor
 public class OrdersService {
     
+    private final OrdersRepository ordersRepository;
+    private final ModelMapper modelMapper;
+
+    public List<OrdersDTO> list(){
+
+        List<Orders> orders = ordersRepository.findAll();
+        List<OrdersDTO> ordersDTOs = orders
+            .stream()
+            .map(order -> modelMapper.map(order, OrdersDTO.class))
+            .collect(Collectors.toList());
+
+        return ordersDTOs;
+    }
+
+
+    public void add(OrdersDTO ordersDTO){
+
+        Orders orders = modelMapper.map(ordersDTO, Orders.class);
+
+        Orders savedOrders = ordersRepository.save(orders);
+    }
 }
