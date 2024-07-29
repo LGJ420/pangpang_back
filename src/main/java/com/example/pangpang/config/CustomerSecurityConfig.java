fa078e3e46a894d77e01413c20c946eed5ccceff
NSB(green) <suh240505@gmail.com>
2024-07-29 15:08:33 +0900

    비밀번호 암호화 bean 작성

--- src/main/java/com/example/pangpang/config/CustomerSecurityConfig.java
+++ src/main/java/com/example/pangpang/config/CustomerSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -54,5 +56,12 @@ public class CustomerSecurityConfig {
 
         return source;
     }
+
+    // ▼▼▼ 비밀번호 암호화 ▼▼▼
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    // ▲▲▲ 비밀번호 암호화 ▲▲▲
 
 }
