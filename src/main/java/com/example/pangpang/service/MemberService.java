ad75241a3f4df38fb446f9065df33026ba267315
NSB(green) <suh240505@gmail.com>
2024-07-30 16:44:21 +0900

    회원가입 비밀번호 암호화

--- src/main/java/com/example/pangpang/service/MemberService.java
+++ src/main/java/com/example/pangpang/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.pangpang.service;
 
 import org.hibernate.mapping.Map;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.pangpang.dto.MemberDTO;
@@ -12,23 +13,30 @@ import lombok.extern.log4j.Log4j2;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService{
+public class MemberService {
     // ↑↑↑↑↑ 사용자의 인증 처리를 위해 인터페이스 임플리먼트
 
     // 멤버 리포지터리 의존성주입
     private final MemberRepository memberRepository;
 
+    // 암호화 의존성 주입
+    private final PasswordEncoder passwordEncoder;
+
     // 회원가입 서비스
-    public void createMember(MemberDTO memberDTO){
+    public void createMember(MemberDTO memberDTO) {
+
+        // 비밀번호 암호화
+        String encoderedPw = passwordEncoder.encode(memberDTO.getMemberPw());
 
         // 리액트 입력값 -> 엔티티 등록값 변경(매핑비스무리)
         Member member = Member.builder()
-            .memberId(memberDTO.getMemberId())
-            .memberPw(memberDTO.getMemberPw())
-            .memberName(memberDTO.getMemberName())
-            .memberBirth(memberDTO.getMemberBirth())
-            .memberRole(memberDTO.getMemberRole())
-            .build();
+                .memberId(memberDTO.getMemberId())
+                // 비밀번호만 암호화 된 거 사용
+                .memberPw(encoderedPw)
+                .memberName(memberDTO.getMemberName())
+                .memberBirth(memberDTO.getMemberBirth())
+                .memberRole(memberDTO.getMemberRole())
+                .build();
 
         memberRepository.save(member);
     }
