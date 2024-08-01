c8731300cc3a6533b8f806b9e602677adfc73840
chomin <doaiso381@naver.com>
2024-08-01 17:37:26 +0900

    메인에 상품 목록 추가하는 중

--- src/main/java/com/example/pangpang/controller/MainPageController.java
+++ src/main/java/com/example/pangpang/controller/MainPageController.java
@@ -1,5 +1,26 @@
 package com.example.pangpang.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pangpang.dto.ProductDTO;
+import com.example.pangpang.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class MainPageController {
+  
+
+  private final ProductService productService;
+
+  @GetMapping("/")
+  public List<ProductDTO> mainProductList() {
+      return productService.mainList();
+  }
   
 }
