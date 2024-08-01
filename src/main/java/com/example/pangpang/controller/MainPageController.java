95d1b502dd0f6023ae9e50cc8430fce5bb99694d
chomin <doaiso381@naver.com>
2024-08-01 17:41:00 +0900

    주석 추가

--- src/main/java/com/example/pangpang/controller/MainPageController.java
+++ src/main/java/com/example/pangpang/controller/MainPageController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pangpang.dto.ProductDTO;
+
 import com.example.pangpang.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
+
+/* 메인페이지에 사용하는 상품 목록 리스트 입니다 */
 
 @RestController
 @RequiredArgsConstructor
