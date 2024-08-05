009f973d7abeb199a60f0083336989fe0bd02822
NSB(green) <suh240505@gmail.com>
2024-08-05 17:40:16 +0900

    리포지터리 정리

--- src/main/java/com/example/pangpang/repository/MemberRepository.java
+++ src/main/java/com/example/pangpang/repository/MemberRepository.java
@@ -7,25 +7,20 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long>{
 
-    Optional<Member> findByMemberId(String memberId);
-
-    // 아이디 찾기에 멤버이름, 멤버생년월일이 필요함
-    public boolean existsByMemberNameAndMemberBirth(String memberName, String memberBirth);
-
+    
+    // 아이디 찾기
+    // 멤버이름, 멤버생년월일이 필요함    
     Member findByMemberNameAndMemberBirth(String memberName, String memberBirth);
-
+    
     // ===============================================================
     
-    // 비밀번호 찾기에 멤버아이디, 멤버이름, 멤버생년월일이 필요함
-    public boolean existsByMemberIdAndMemberNameAndMemberBirth(String memberId, String memberName, String memberBirth);
-    
+    // 비밀번호 찾기
+    // 멤버아이디, 멤버이름, 멤버생년월일이 필요함
     Member findByMemberIdAndMemberNameAndMemberBirth(String memberId, String memberName, String memberBirth);
     
     // ===============================================================
     
-    // 로그인에 멤버 아이디, 멤버 비밀번호 필요함
-    Optional<Member> findByMemberIdAndMemberPw(String memberId, String MemberPw);
-
-    // 로그인에 멤버 아이디, 멤버 비밀번호 유무 알기 위함
-    public boolean existsByMemberIdAndMemberPw(String memberId, String memberPw);
+    // 로그인
+    // 멤버 아이디, 멤버 비밀번호 필요함
+    Optional<Member> findByMemberId(String memberId);
 }
