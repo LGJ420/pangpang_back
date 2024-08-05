fa1e11e2762bac9bc71f979930edef5b88361a29
chomin <doaiso381@naver.com>
2024-08-05 17:10:54 +0900

    설명 주석 추가 및 사진 업로드 테스트

--- src/main/java/com/example/pangpang/controller/ProductController.java
+++ src/main/java/com/example/pangpang/controller/ProductController.java
@@ -1,24 +1,45 @@
 package com.example.pangpang.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.pangpang.dto.PageRequestDTO;
 import com.example.pangpang.dto.PageResponseDTO;
 import com.example.pangpang.dto.ProductDTO;
 import com.example.pangpang.service.ProductService;
+import com.example.pangpang.util.CustomFileUtil;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequiredArgsConstructor
+@Log4j2
 @RequestMapping("/api/product")
 public class ProductController {
 
   private final ProductService productService;
+  private final CustomFileUtil fileUtil;
+
+  @PostMapping("/")
+  public Map<String, String> register(ProductDTO productDTO) {
+    log.info("register : " + productDTO);
+    List<MultipartFile> files = productDTO.getFiles();
+
+    List<String> uploadFileNames = fileUtil.saveFiles(files);
+    log.info(uploadFileNames);
+
+    return Map.of("RESULT", "SUCCESS");
+  }
+
 
   /* 상품 목록 보기 */
   @GetMapping("/list")
--- src/main/java/com/example/pangpang/dto/ProductDTO.java
+++ src/main/java/com/example/pangpang/dto/ProductDTO.java
@@ -1,5 +1,9 @@
 package com.example.pangpang.dto;
 
+import java.util.*;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -22,10 +26,10 @@ public class ProductDTO {
     @Positive
     private int productPrice;
 
-    // @Builder.Default
-    // private List<MultipartFile> files = new ArrayList<>();
+    @Builder.Default
+    private List<MultipartFile> files = new ArrayList<>();
 
-    // @Builder.Default
-    // private List<String> uploadFileNames = new ArrayList<>();
+    @Builder.Default
+    private List<String> uploadFileNames = new ArrayList<>();
 
 }
