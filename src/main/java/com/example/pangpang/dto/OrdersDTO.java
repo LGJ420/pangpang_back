89ca7d384a56e2a9be61383fc223b9cd222bb625
LGJ420 <leegj420@gmail.com>
2024-08-09 16:19:57 +0900

    포메터 제거, @JsonFormat로 포멧

--- src/main/java/com/example/pangpang/dto/OrdersDTO.java
+++ src/main/java/com/example/pangpang/dto/OrdersDTO.java
@@ -1,7 +1,9 @@
 package com.example.pangpang.dto;
 
 import java.util.*;
+import java.time.*;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.*;
@@ -22,6 +24,9 @@ public class OrdersDTO {
     @NotEmpty
     @Pattern(regexp = "^[0-9]*$", message = "숫자(0~9)만 입력 가능합니다.")
     private String phone;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime orderDate;
 
     @JsonProperty("dtoList")
     private List<OrdersProductDTO> ordersProducts;
