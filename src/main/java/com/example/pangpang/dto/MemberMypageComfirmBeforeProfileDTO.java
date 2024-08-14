4c893aacf45c8288a04ba119735dc9672ef9ca7e
NSB(green) <suh240505@gmail.com>
2024-08-14 10:43:01 +0900

    사용하지 않는 dto 삭제

--- src/main/java/com/example/pangpang/dto/MemberMypageComfirmBeforeProfileDTO.java
+++ src/main/java/com/example/pangpang/dto/MemberMypageComfirmBeforeProfileDTO.java
@@ -1,7 +1,6 @@
 package com.example.pangpang.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +13,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberMypageComfirmBeforeProfileDTO {
-
-    @NotNull
-    private String memberIdInConfirmBeforeProfile;
 
     @NotNull(message = "비밀번호는 필수 항목입니다.")
     private String memberPwInConfirmBeforeProfile;
