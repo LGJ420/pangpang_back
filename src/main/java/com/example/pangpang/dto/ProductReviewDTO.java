d5294231c9b21e477b70baf8a312512fd15243f6
LGJ420 <leegj420@gmail.com>
2024-08-15 19:14:12 +0900

    리뷰 시간값 포멧

--- src/main/java/com/example/pangpang/dto/ProductReviewDTO.java
+++ src/main/java/com/example/pangpang/dto/ProductReviewDTO.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.*;
 
 @Data
@@ -18,6 +20,7 @@ public class ProductReviewDTO {
     private MultipartFile reviewFile;
     private String reviewFileName;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime reviewDate;
 
     private Long productId;
