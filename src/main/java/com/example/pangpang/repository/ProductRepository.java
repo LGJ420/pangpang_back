b2be6b397f8f735eb9d9b4625286f43df16843a7
chomin <doaiso381@naver.com>
2024-09-04 14:42:55 +0900

    상품 품절 시 코드 수정

--- src/main/java/com/example/pangpang/repository/ProductRepository.java
+++ src/main/java/com/example/pangpang/repository/ProductRepository.java
@@ -19,7 +19,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
   // 쇼핑 페이지 상품 목록
   @Query("SELECT p FROM Product p WHERE (:category IS NULL OR :category = '' OR p.productCategory = :category) " +
-      "AND (:search IS NULL OR :search = '' OR p.productTitle LIKE %:search%) " + "AND p.productStock > 0" +
+      "AND (:search IS NULL OR :search = '' OR p.productTitle LIKE %:search%) " +
       "ORDER BY p.id DESC")
   Page<Product> findProductsByCategoryAndSearch(@Param("category") String category,
       @Param("search") String search,
